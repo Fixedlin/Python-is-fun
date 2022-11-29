29ea7cad3386ecca20d5db0bc5d3178834ff6e59
rinosetiyo <42023075+rinosetiyo@users.noreply.github.com>
2022-11-29 10:56:50 +0700

    Update rock-paper-scissors.py

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -25,36 +25,55 @@ scissors = '''
 ---.__(___)
 '''
 
-game_image = [rock , paper , scissors]
+game_images = [rock, paper, scissors]
 
 # Write code below
 import random
+from replit import clear
 
-print("Game rock paper scissors")
-print("Cara bermainnya 0 untuk batu 1 untuk kertas 2 untuk gunting")
-print("==============================")
+def play_game():
+  print("Game rock paper scissors")
+  print("Rules of game 0 for rock, 1 for paper, 2 for scissors")
+  print("---------------------------------")
+  users = int(input("Input your choice : "))
+  
+  if users > 2 or users < 0:
+    print("Invalid input")
+  else:
+    computer = random.randint(0, 2)
+    print(f"Your choice is : {game_images[users]}")
+    print(f"Computer choice is : {game_images[computer]}")
+  
+    if users == 0 and computer == 1:
+      print("You lose")
+    elif users == 0 and computer == 2:
+      print("You win")
+    elif users == 1 and computer == 0:
+      print("You win")
+    elif users == 1 and computer == 2:
+      print("You lose")
+    elif users == 2 and computer == 0:
+      print("You lose")
+    elif users == 2 and computer == 1:
+      print("You win")
+    elif users == computer:
+      print("Draw !")
 
-users = int(input("Masukkan pilihan anda : "))
+def try_again():
+  answer = input("Did you want to play again Y/N ? : ").lower()
+  
+  if answer == "n":
+    clear()
+    print("Thank you, you've played this game !!!")
+  else:
+    clear()
+    while answer == "y":
+      play_game()
+      answer = input("Did you want to play again Y/N ? : ")
+      clear()
+      if answer == "n":
+        clear()
+        print("Thank you, you've played this game !!!")
 
-if users > 2 or users < 0:
-  print("Invalid number, You lose")
-else:
-  print(f"Anda memilih : {game_image[users]}")
-  computer = random.randint(0,2)
-  print(f"Komputer memilih : {game_image[computer]}")
-
-
-  if users == 0 and computer == 1:
-    print("Computer win")
-  elif users == 0 and computer == 2:
-    print("You win")
-  elif users == 1 and computer == 0:
-    print("You win")
-  elif users == 1 and computer == 2:
-    print("Computer win")
-  elif users == 2 and computer == 0:
-    print("You win")
-  elif users == 2 and computer == 1:
-    print("You win")
-  elif users == computer:
-    print("Draw !")
+play_game()
+try_again()
